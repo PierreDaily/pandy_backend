ee91047ef280cff461fd4d3c809b1a3ab4628ef8
Pierre DAILY <contact@pierredaily.com>
2021-01-18 16:11:12 +0800

    Top level async added

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,19 @@ import { accessTokenMiddleWare, permissionMiddleware } from "./middleware";
 
 const port = process.env.API_PORT;
 
-// Top level await needed here
-createConnection();
+(async () => {
+  await createConnection();
+  const app = express();
+  app.use(passport.initialize());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-const app = express();
-app.use(passport.initialize());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.post("/auth", controller.auth.authenticateUser);
+  app.use(accessTokenMiddleWare);
 
-app.post("/auth", controller.auth.authenticateUser);
-app.use(accessTokenMiddleWare);
+  app.use("/brand", router.brand);
+  app.use("/branch", router.branch);
+  app.use("/user", router.user);
 
-app.use("/brand", router.brand);
-app.use("/branch", router.branch);
-app.use("/user", router.user);
-
-app.listen(port, () => console.log(`myapi listening on port ${port}!`));
+  app.listen(port, () => console.log(`myapi listening on port ${port}!`));
+})();
